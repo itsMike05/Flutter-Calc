33c4703e46cb0b921a8f368e5fdf45a1a5eac5e1
Michał Pelc <michal.pelc@live.com>
2023-02-08 23:06:45 +0100

    Created a buildButton Widget

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,23 @@ class SimpleCalc extends StatefulWidget {
 }
 
 class _SimpleCalcState extends State<SimpleCalc> {
+  Widget buildButton(
+      String buttonText, double buttonHeight, Color buttonColor) {
+    return Container(
+      // Covering 10% of the device's height
+      height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
+      color: buttonColor,
+      child: TextButton(
+        onPressed: null,
+        child: Text(buttonText,
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 36,
+                color: Colors.white)),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,16 +60,37 @@ class _SimpleCalcState extends State<SimpleCalc> {
             padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
             child: const Text(
               "0",
-              style: TextStyle(fontSize: 36),
+              style: TextStyle(fontSize: 36.0),
             ),
           ),
           Container(
             alignment: Alignment.centerRight,
-            padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+            padding: const EdgeInsets.fromLTRB(10, 30, 10, 0),
             child: const Text(
               "0",
-              style: TextStyle(fontSize: 36),
+              style: TextStyle(fontSize: 48.0),
             ),
+          ),
+          const Expanded(child: Divider()),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                // Covering 75% of the device's width
+                width: MediaQuery.of(context).size.width * 0.75,
+                child: Table(
+                  children: [
+                    TableRow(
+                      children: [
+                        buildButton("C", 1, Colors.redAccent),
+                        buildButton("Del", 1, Colors.blueAccent),
+                        buildButton("/", 1, Colors.blueGrey)
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ],
       ),
